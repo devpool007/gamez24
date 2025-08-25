9b4b3182e2020ad590348efa412c59c92cfb382b
devpool007 <sharmadevansh007777@gmail.com>
2025-08-25 17:39:47 +0200

    fix getCountry #1

--- lib/actions/country-action.ts
+++ lib/actions/country-action.ts
@@ -8,7 +8,11 @@ interface CountryResponse {
 
 export async function getCountry(): Promise<CountryResponse> {
   const headersList = headers();
-  const country: string = (await headersList).get("x-user-country") || "DE";
+  const country: string =
+    (await headersList).get("x-user-country") ||
+    (await headersList).get("x-vercel-ip-country") ||
+    (await headersList).get("cf-ipcountry") ||
+    "DE";
 
   return { country };
 }
