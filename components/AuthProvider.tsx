2e580ec50ce0faa1bc8f4e2cc95f3b9d72d5468e
devpool007 <sharmadevansh007777@gmail.com>
2025-09-23 13:47:08 +0200

    added new loading text for server

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -3,15 +3,26 @@
 
 import { useAuth } from "@/hooks/useAuth";
 
-export default function AuthProvider({ children }: { children: React.ReactNode }) {
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const { loading } = useAuth();
 
   if (loading) {
     // Optional: put a full-screen loader here
     return (
-      <div className="flex h-screen items-center justify-center bg-background">
-        <span className="animate-pulse text-3xl text-foreground font-gaming">Loading...</span>
-      </div>
+      <>
+        <div className="flex flex-col h-screen items-center justify-center bg-background">
+            <div className="animate-pulse text-3xl text-foreground font-gaming">
+            Loading...
+            </div>
+            <div className="animate-pulse text-sm text-foreground font-modern mt-2">
+            Please have patience the backend server was asleep. This may take a moment.
+            </div>
+        </div>
+      </>
     );
   }
 
